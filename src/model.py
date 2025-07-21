f3867f88a3b1728517be17c083b9d8ca754bb8ce
TaehyunJeon <jeontaehyun0203@gmail.com>
2025-07-21 15:12:41 +0900

    Feat: 모델 학습 구현

--- src/model.py
+++ src/model.py
@@ -1,10 +1,17 @@
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
 
-def model_init():
-    pass
 
-def model_training():
-    print("모델 훈련")
+def model_init(df):
+    X = df.iloc[:, '메일제목']
+    y = df.iloc[:, '메일종류']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    return X_train, X_test, y_train, y_test
 
-def model_evaluation():
-    print("모델 평가")
+def model_training(X_train, X_test, y_train, y_test):
+    model = LogisticRegression()
+    model.fit(X_train, y_train)
+    return model
+
+def model_evaluation(X_test, y_test, model):
+    model.score(X_test, y_test)
