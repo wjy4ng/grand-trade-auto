80e6e2791607ae3242518c7fffac5f837a9ab868
wjy4ng <83898622+wjy4ng@users.noreply.github.com>
2025-07-23 13:23:25 +0900

    feat: form -> predict 앱 이름 변경

--- server/predict/apps.py
+++ server/predict/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class FormConfig(AppConfig):
+class PredictConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'form'
+    name = 'predict'
--- server/server/urls.py
+++ server/server/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(form.urls)),
+    path('api/predict-price/', include('predict.urls')),
 ]
